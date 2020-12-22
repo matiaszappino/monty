e82a7193d44481248bca759b4c5a87e9abfb6f05
Matias Zappino <matiaszappino31@gmail.com>
2020-12-22 19:17:37 +0000

    Fixed Sub Function

--- functions_b.c
+++ functions_b.c
@@ -47,7 +47,7 @@ void sub_function(stack_t **stack, unsigned int line_number)
 	stack_t *aux = NULL;
 	stack_t *aux_two = NULL;
 
-	if (!stack || !(*stack)->next)
+	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
