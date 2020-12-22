98995b5b8750e40bfde3d376ea088ede452ba832
Matias Zappino <matiaszappino31@gmail.com>
2020-12-22 19:21:57 +0000

    Fixed Div Function

--- functions_b.c
+++ functions_b.c
@@ -71,7 +71,7 @@ void div_function(stack_t **stack, unsigned int line_number)
 	stack_t *aux = NULL;
 	stack_t *aux_two = NULL;
 
-	if (!*stack || !(*stack)->next)
+	if (!*stack && !(*stack)->next)
 	{
 	fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
